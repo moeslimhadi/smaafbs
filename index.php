22dcb526ebd44eb1377174e1fc4ef9fd8e03e1ac
moeslimhadi <moeslimhadi@gmail.com>
2021-02-13 21:43:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PPDB ONLINE - SMA Al Furqon Boarding School (AFBS)</title>
+    <!--[if IE 8]>
+    <link href="style_ie8.css" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="fonts/awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href='css/owl.carousel.css' rel='stylesheet' type='text/css'>
+    <link href='css/owl.theme.css' rel='stylesheet' type='text/css'>
+    <link href="style.css" rel="stylesheet" />
+	
+<link rel="icon" type="image/png" href="logo/logo.png">
+</head>
+<body>
+    <header class="tz-header tz-header-2">
+	   <!--
+        <div class="tz-sign-in">
+            <div class="container">
+                <p>SELAMAT DATANG DI APLIKASI PPDB ONLINE SMA Al Furqon Boarding School (AFBS)</p>
+            </div>
+        </div>
+		-->
+        <div class="tz-customer-container">
+            <div class="container">
+                <div class="tz-header-content">
+                    <h3 class="tz-logo pull-left">
+                        <a href="#"><img alt="Images Logo" src="demos/logo-afbs-header2.png"></a>
+                    </h3>
+                    <button type="button" class="tz-button-toggle btn-navbar pull-right" data-target=".nav-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                     <?php 
+                        include("top_menu.php");
+                    ?>
+                </div>
+            </div><!-- End Content Main Header -->
+        </div>
+    </header>
+    <!-- End class tz-header-2 -->
+
+    <section class="tz-banner tz-banner-style2">
+        <!-- Begin class banner style-3 -->
+        <div class="tz-banner-style2">
+            <div class="tz-slider-banner">
+                <div class="tz-items">
+                    <div class="tz-slider-images">
+                        <img src="demos/Banner/afbs_terang.jpg" alt="Images">
+                    </div>
+                    <div class="tz-banner-content">
+                        <div class="container">
+                            <!-- <small>NUMBER ONE</small> -->
+                            <h4>SMA Al Furqon Boarding School (AFBS)</h4>
+                            <span class="tz-under-line"></span>
+                            <h6>Aplikasi PPDB Online</h6>
+                        </div>
+						<div>
+                        <a href="https://api.whatsapp.com/send?phone=628176040490" ><img height="100" width="200" src="demos/waafif.png" alt="Images">
+                        </a>
+						 <a href="https://api.whatsapp.com/send?phone=628176040490" ><img height="100" width="200" src="demos/waria.png" alt="Images">
+                        </a>
+						 <a href="https://api.whatsapp.com/send?phone=628176040490" ><img height="100" width="200" src="demos/waagie.png" alt="Images">
+                        </a>
+						</div>
+                        <!-- <marquee style="font-family: Arial; font-size:15px; color:#FFFAF0;"><strong> ..... PANDUAN PENGISIAN FORMULIR ONLINE ------> Regristasi Terlebih Dahulu Klik Menu MENDAFTAR, Setelah Itu Melengkapi BIODATA DIRI Klik Menu LOGIN .....</strong></marquee> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End  class banner style-3 -->
+    </section>
+
+    <!-- Begin section tz-introduce-univesity -->
+    <section class="tz-introduce-univesity">
+         <?php 
+            include("btn_4.php");
+        ?>
+        <div class="tz-introduce-content" style="text-align: justify;">
+            <h1><b>SMA Al Furqon Boarding School Tangerang</b></h1>
+			SMA Al Furqon Boarding School (AFBS) adalah sekolah Menengah Atas yang beroperasi mulai tanggal 01 Januari 2021 di bawah naungan Yayasan DAR Al Furqon Al Hakim. Adapun Pendirian SMA AFBS bertujuan untuk ikut serta mensukseskan program Pendidikan Nasional yang berfungsi mengembangkan kemampuan dan membentuk watak serta peradaban bangsa yang bermartabat dalam rangka mencerdaskan kehidupan bangsa.<br />
+			<h3><b>Profil Sekolah</b></h3>
+			SMA Al Furqon Boarding School (AFBS) merupakan salah satu sekolah yang terletak di Jalan Untung Suropati 1 No 13 Cimone Jaya Karawaci Kota Tangerang, sekolah ini sendiri dilengkapi dengan berbagai fasilitas yang ada dan juga melengkapi sekolah ini bagaimana layaknya sekolah ini sendiri. berbagai kegiatan sekolah yang juga ada dalam sekolah ini melengkapi kelengkapan sekolah ini sendiri, dimana hal itu adalah termasuk dalam kegiatan akademis maupun kegiatan yang berupa non akademis. Dimana semuanya itu adalah termasuk dari bagaimana sistem pengajaran yang ada dalam sekolah ini, juga bagaimana sistem guru yang ada dalam sekolah ini sendiri, sistem guru yang ada disini sendiri mampu melakukan beberapa hal yang sangat baik bagi prestasi sekolah maupun beberapa siswa yang ada disini sehingga membuat sekolah ini menjadi lebih baik dan lebih bagus lagi.<br />
+
+			<h3><b>Prestasi</b></h3>
+			Prestasi merupakan suatu hal yang sangat membanggakan sekolah, karena hal ini dapat membuat suatu nama sekolah menjadi baik atau bisa juga membuat nama suatu sekolah menjadi buruk, pada SMA Al Furqon Boarding School Tangerang ini sendiri mempunyai beberapa prestasi, walaupun nama sekolah ini sangat jarang tersebut dalam lingkungan sekolah yang ada di seluruh Indonesia, tapi para adik-adik tidak boleh berkecil hati atau kecewa, karena pada dasarnya sekolah ini memiliki beberapa sistem pengajaran yang baik dan juga dapat membuat nilai ujian nasional menjadi sangat baik. Hal ini sendiri adalah berdasarkan dari bagaimana pengajaran yang ada pada sekolah ini yang membuat prestasi dari peserta didik menjadi meningkat. Prestasi yang pernah dicapai dalam sekolah ini sendiri adalah dalam bidang pelajaran seperti pada halnya olimpiade sains, cerdas cermat dan bidang-bidang akademis lainnya, sedangkan dalam bidang non akademis sendiri lebih mengutamakan bagaimana melakukan kegiatan olahraga, karena dalam hal ini sekolah ini sendiri sangat mengutamakan bagaimana kegiatan olahraga yang ada dalam sekolah ini sendiri, sehingga kita dapat melihat bahwa prestasi banyak dihasilkan dari kegiatan olahraga dan juga dapat menunjang berbagai prestasi yang ada dalam sekolah ini sendiri, sehingga membuat sekolah ini menjadi lebih baik lagi dan semakin baik lagi setiap tahunnya.
+        </div>
+    </section>
+    <!-- End section tz-introduce-univesity -->
+
+    <!-- Begin section tz-portfolio-wrapper -->    
+    
+    <!-- End section tz-lastest-event -->
+
+    <!-- Begin section tz-contact-us -->
+    
+    <!-- End section tz-contact-us -->
+
+    <!-- Begin Footer -->
+    <section class="tz-introduce-univesity" style="width: 100%;background: #334878">
+        <div class="tz-cource-services tz-cource-services-style-2">
+            <ul>
+                <li>
+                    <div class="tz-background-color-4" style="text-align: center">
+                       <h3 style="color: white">PPDB ONLINE</h3><hr />
+                       <font color="white">Sistem Informasi Penerimaan Siswa Baru SMA Al Furqon Boarding School Tangerang Merupakan sistem informasi berbasis web yang dibangun menggunakan Bahasa Pemrograman PHP database MySQL. Sedangkan Front End dari Sistem Informasi ini menggunakan Framework Materializecss dan Bootstrap agar tampilan terlihat Menarik</font>
+                    </div>
+                </li>
+                <li>
+                    <div class="tz-background-color-4" style="text-align: center">
+                        <h3 style="color: white">MANFAAT</h3><hr style="border-color: #334878" />
+                        <font color="white">Dengan adanya Sistem Informasi PPDB Online di SMA Al Furqon Boarding School Tangerang diharapkan dapat membantu Calon siswa untuk melakukan Pendaftaran Tanpa harus datang ke sekolah untuk melakukan pendaftaran, menghemat biaya dan Waktu.</font>
+                    </div>
+                </li>
+                <li>
+                   <div class="tz-background-color-4" style="text-align: center">
+                        <h3 style="color: white">SMA CIFON</h3><hr />
+                        <font color="white">Merupakan salah satu Sekolah tingkat SMA yang terletak di Cimone Jaya, Karawaci, Kota Tangerang. tentu saja sekolah ini tidak akan kalah dengan sekolah-sekolah lainnya yang mempunyai akreditasi lebih baik.</font>
+                    </div>
+                </li>
+                <li>
+                    <div class="tz-background-color-4" style="text-align: center">
+                        <h3 style="color: white">LOKASI </h3><hr style="border-color: #334878" />
+                        <font color="white">Jl. Untung Suropati 1 No 13<br/> Cimone Jaya, Karawaci<br /> Kota Tangerang, Banten 15114</font>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <footer class="tz-footer">
+       
+        <div class="tz-footer-address-site">
+            <address> Hak Cipta SMA Al Furqon Boarding School Tangerang @ <?php  date_default_timezone_set("Asia/Jakarta");$thn=date("Y");echo"$thn";?> | Program Pengembangan PPDB Oleh Tim IT Yayasan Dar Al Furqon Al Hakim</address>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <script>
+        var Desktop       =   5,
+            tabletportrait    =   2,
+            mobilelandscape   =   1,
+            mobileportrait    =   1,
+            resizeTimer       =   null;
+    </script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/off-canvas.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/resize.js"></script>
+    <script src="js/custom-portfolio.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.parallax-1.1.3.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
